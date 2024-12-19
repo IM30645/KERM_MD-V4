75846b84e4a245a6ab1f4b93f426fa71d737b472
IM30645 <ndingadephez@gmail.com>
2024-12-19 17:24:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
+Niconst fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ENTER YOUR SESSION HERE",
+SESSION_ID: process.env.SESSION_ID || "KNYlUSrT#g7UCuUVBqlZJxA6hoLFmrumW-goCZyHM6-PG0MhcSoc",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
